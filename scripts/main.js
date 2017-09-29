70ca509537a964e7e408af51fb62ee01696875a2
FlorianDesjouis <floriandesjouis@supinternet.fr>
2017-09-29 16:43:54 +0200

    added remaining func to see dynamicly how many tasks left todo

---
+++ scripts/main.js
@@ -0,0 +1,26 @@
+{
+    'use strict';
+
+    new Vue({
+        el: 'main#app',
+        data: {
+            tasks: [
+                {title: "Grab some popcorn", isDone: true},
+                {title: "Watch Rick and Morty", isDone: true},
+                {title: "Go to the shower", isDone: false}
+            ]
+        },
+
+        methods: {
+            remaining: function () {
+                var remains = 0;
+                this.tasks.forEach(function (task) {
+                    if (task.isDone === false) {
+                        remains++;
+                    }
+                });
+                return remains;
+            }
+        }
+    });
+}
